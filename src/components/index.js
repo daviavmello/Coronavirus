fcc0d4fab494946a5609f65d3effcfc117a50854
Davi Mello <davimello1994@gmail.com>
2020-05-13 16:43:57 -0400

    Cards, Chart, CountryPicker components created

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// Makes it possible to export Components to App.js in a cleaner way.
+
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
