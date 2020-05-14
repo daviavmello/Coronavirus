1509f930ee68a5c938154721be09b161afecaa34
Davi Mello <davimello1994@gmail.com>
2020-05-14 16:36:10 -0400

    Added Material UI to components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 // Hooks vs. Class-based. Decided to go for class-based for App.js only.
 class App extends React.Component {
+
+  state = {
+    data: {},
+  }
+
+// componentDidMount requires async to comes first
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    
+    this.setState( { data: fetchedData }) 
+  }
   render(){
+    // const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data = {this.state.data} />
         <CountryPicker />
         <Chart />
       </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css';
 
-const Cards = () => {
+// props is what's being passed in, in this case, data from the API
+const Cards = (props) => {
+    console.log(props);
+    
+    
     return(
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+              <Grid item component={Card}>
+                <CardContent>
+                  <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                  <Typography variant="h5">Real Data</Typography>
+                  <Typography color="textSecondary">Real Date</Typography>
+                  <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                </CardContent>
+              </Grid>
+            </Grid>
+
+        </div>
     )
 }
 
